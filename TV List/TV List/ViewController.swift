ca80b06ff35f95580552376e99211984dc2da6fe
Dan Munoz <danmunoz@me.com>
2018-02-28 18:11:28 -0400

    Base project created

---
+++ TV List/TV List/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TV List
+//
+//  Created by Daniel Munoz on 2/28/18.
+//  Copyright © 2018 Daniel Munoz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
